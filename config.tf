872981f24da0ac300a07ae3f83401b65a7a0280d
Karthiklearning <107940151+Karthiklearning@users.noreply.github.com>
2023-10-11 11:35:09 +0530

    Update config.tf

--- config.tf
+++ config.tf
@@ -1,3 +1,13 @@
 provider "aws" {
-    region = "${var.AWS_REGION}"
+  region = "us-west-2"  # Set to US West (Oregon)
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "popeye007"
+  acl    = "private"
+}
+
+variable "s3_bucket_name" {
+  description = "Name of the S3 bucket"
+  default     = "popeye007"
 }
